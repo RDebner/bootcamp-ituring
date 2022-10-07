0fb13ee27b82fd6a3154e7b769e68da755ed5876
Rafael <rafaeldebner08@gmail.com>
2022-10-07 20:31:57 -0300

    adicionando comandos pratica 1

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -2,4 +2,6 @@ let nome = 'Rafael'
 let email = 'rafaeldebner08@gmail.com'
 let profissão = 'estudante'
 let idade = 13
-let lugar = Chile
+let lugar = 'Chile'
+let frase = ` ${nome} ${email} ${profissão} ${idade} ${lugar}`
+console.log(frase)
