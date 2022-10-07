2a9d636ab484fae19c340977ea7d45fecf8bc861
Rafael <rafaeldebner08@gmail.com>
2022-10-07 20:34:56 -0300

    adicionando tipos de variáveis

--- mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-1.js
@@ -3,5 +3,5 @@ let email = 'rafaeldebner08@gmail.com'
 let profissão = 'estudante'
 let idade = 13
 let lugar = 'Chile'
-let frase = ` ${nome} ${email} ${profissão} ${idade} ${lugar}`
+let frase = ` ${typeof(nome)} ${typeof(email)} ${typeof(profissão)} ${typeof(idade)} ${typeof(lugar)}`
 console.log(frase)
