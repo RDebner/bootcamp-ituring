29d8bbeb8a3d2357ace266ba0de0be7ecf1dc873
Rafael <rafaeldebner08@gmail.com>
2022-10-10 11:28:17 -0300

    true e false da variável

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-3.js
@@ -0,0 +1,7 @@
+let nome = 'Rafael'
+let email = 'rafaeldebner08@gmail.com'
+let profissao = 'estudante'
+let idade = 13
+let lugar = 'Chile'
+let frase = ` ${'string' == typeof(nome)} ${'string' == typeof(email)} ${'string' == typeof(profissao)} ${'string' == typeof(idade)} ${'string' == typeof(lugar)}`
+console.log(frase) 
