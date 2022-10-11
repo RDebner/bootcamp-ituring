5723c801cce2223be138f16f03fd4aa62102c570
Rafael <rafaeldebner08@gmail.com>
2022-10-11 00:18:18 -0300

    adicionando mensagem da agência de turismo Viagem dos sonhos

---
+++ mes-1-logica-de-programacao/tarefa-semana-1/pratica-5.js
@@ -0,0 +1,7 @@
+let nome = 'Rafael'
+let email = 'rafaeldebner08@gmail.com'
+let profissão = 'estudante'
+let idade = 13
+let lugar = 'Chile'
+let frase = ` Olá,${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para o ${lugar}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissão} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`
+console.log(frase)
