2ae70acdcf24b3fadad4be40e64bc9c034ba7a51
Rafael <rafaeldebner08@gmail.com>
2022-10-16 21:59:41 -0300

    porcentagem de desconto no produto

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-4.js
@@ -0,0 +1,15 @@
+function quantidadeDeDesconto (valorDoProduto,porcentagemDeDesconto) {
+    valorDoProduto = 500
+    let min = 1
+    let max = 100
+    porcentagemDeDesconto = Math.random()*(max-min) + 1
+    let desconto = porcentagemDeDesconto
+    let desconto2 = parseInt(desconto)
+    console.log(`${desconto2}%`)
+    let calculo = valorDoProduto/100
+    let calculo2 = calculo*desconto2
+console.log(calculo2)
+}
+
+
+quantidadeDeDesconto()
