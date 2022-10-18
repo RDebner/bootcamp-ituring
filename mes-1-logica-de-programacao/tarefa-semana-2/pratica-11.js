c70ec9bc2f761fc21056cc50dfad0546ea3fb0d3
Rafael <rafaeldebner08@gmail.com>
2022-10-18 11:32:38 -0300

    rgb das cores

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-11.js
@@ -0,0 +1,30 @@
+function cores (cor) {
+    cor = "verde"
+    switch (cor) {
+        case "vermelha":
+            console.log("O RGB da cor vermelha é rgb(255,0,0)")
+            break;
+        case "laranja":
+            console.log("O RGB da cor laranja é rgb(255,128,0)")
+            break;
+        case "amarela" :
+            console.log("O RGB da cor amarela é rgb(255,255,0)")
+            break;
+        case "verde":
+            console.log("O RGB da cor verde é rgb(,255,)")
+            break;
+        case "azul claro":
+            console.log("O RGB da cor azul claro é rgb(0,255,255)")
+            break;
+        case "azul marinho":
+            console.log("O RGB da cor azul marinho é rgb(a,a,255)")
+            break;
+        case "rosa":
+            console.log("O RGB da cor rosa é rgb(255,0,255)")
+            break;  
+            default:    
+            console.log("O RGB padrão é rgb(255,255,255)")
+    }
+
+}
+cores()
