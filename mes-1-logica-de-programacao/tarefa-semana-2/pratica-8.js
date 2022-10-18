43a82c460dac6154f50de4a725e7d2aa5f5cdd2a
Rafael <rafaeldebner08@gmail.com>
2022-10-17 23:26:41 -0300

    número par ou ímpar

---
+++ mes-1-logica-de-programacao/tarefa-semana-2/pratica-8.js
@@ -0,0 +1,11 @@
+function numeroParOuImpar (numero) {
+numero = 4
+if (numero%2==0) {
+    console.log(`O número ${numero} é par`)
+} else {
+    console.log(`O número ${numero} é impar`)
+}
+
+}
+
+numeroParOuImpar()
