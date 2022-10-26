d00fd752d6c7332b58c52920e4259e643faa9cdb
Rafael <rafaeldebner08@gmail.com>
2022-10-25 21:48:47 -0300

    lista séries

---
+++ mes-1-logica-de-programacao/tarefa-semana-3/pratica-4.js
@@ -0,0 +1,7 @@
+function séries (listaSéries) {
+    for (let i = 0; i < listaSéries.length; i++)
+        console.log(`[${i}] ${listaSéries[i]}`)
+}
+
+const listaSéries = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
+séries(listaSéries)
