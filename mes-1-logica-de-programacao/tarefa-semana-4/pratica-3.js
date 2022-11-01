431a5dc303e09d8b69f824f4a3acf8c130c1fe4d
Rafael <rafaeldebner08@gmail.com>
2022-11-01 13:16:40 -0300

    termo de busca série melhorado

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-3.js
@@ -0,0 +1,6 @@
+ let lista = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
+ let termoDeBusca = "Th" 
+let listaFiltrada = lista.filter((item) => item.includes(termoDeBusca))
+console.log(listaFiltrada)
+
+
