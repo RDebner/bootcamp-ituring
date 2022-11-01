dce4798c74b9df963db5e2287c121df5b1f13df1
Rafael <rafaeldebner08@gmail.com>
2022-11-01 00:17:25 -0300

    serie lista melhorada

--- mes-1-logica-de-programacao/tarefa-semana-4/pratica-2.js
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-2.js
@@ -1,10 +1,12 @@
 let listaDeSéries = ["Breaking Bad", "Game of Thrones", "Stranger Things", "Chernobyl", "Dexter", "The Last Dance", "This is Us"]
-let serie = "Game of Thrones"
+let serie = "Dexter"
 
 
 
 listaDeSéries.forEach(function(listaDeSéries,serieDesejada) {
 if(listaDeSéries == serie){
     console.log(serieDesejada)
+} else {
+    return undefined
 }
 })
