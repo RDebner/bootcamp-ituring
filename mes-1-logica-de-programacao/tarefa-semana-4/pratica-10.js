18f2b75fc8a0d86e175b1197aebe8b97d098253d
Rafael <rafaeldebner08@gmail.com>
2022-11-03 19:59:48 -0300

    Calculando o saldo Final

---
+++ mes-1-logica-de-programacao/tarefa-semana-4/pratica-10.js
@@ -0,0 +1,57 @@
+let lista = [{
+    valor: 100.00,
+    movimentacao: "deposito",
+    dataMovimentacao: "2021-08-02T07:46:36.611Z"
+  },
+  {
+    valor: 20.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2021-09-17T09:46:36.611Z"
+  },
+  {
+    valor: 30.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-02-24T09:46:36.611Z"
+  },
+  {
+    valor: 10.00,
+    movimentacao: "deposito",
+    dataMovimentacao: "2022-03-02T09:46:36.611Z"
+  },
+  {
+    valor: 7.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-04-20T09:46:36.611Z"
+  },
+  {
+    valor: 21.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-03-20T09:46:36.611Z"
+  },
+  {
+    valor: 5.00,
+    movimentacao: "retirada",
+    dataMovimentacao: "2022-06-08T09:46:36.611Z"
+  }]
+
+  function exibeValorFinal () {
+    let valorFinal = 0
+    for (item of lista)
+    if (item.movimentacao === "deposito"){
+        valorFinal += item.valor
+    }
+    if (item.movimentacao === "retirada"){
+        valorFinal -= item.valor
+    }
+
+    function positivoOuNegativo (valorFinal){
+       positivoNegativo = valorFinal >= 0 ? "positivo" : "negativo"
+       return positivoNegativo
+   }
+   console.log(`O saldo final de sua conta foi positivo no valor de R$ 27.`)
+}
+
+   
+
+
+  exibeValorFinal(lista)
